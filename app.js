60dffce8c800c2e600232efade4f096c0a167646
larrissalira <larrissalira@outlook.com>
2022-07-22 12:12:40 -0300

    criando arquivos app.js e gitignore

---
+++ app.js
@@ -0,0 +1,19 @@
+import express from "express";
+
+import * as dotenv from "dotenv";
+
+import Dicas from "./src/controllers/Dicas.js";
+
+dotenv.config();
+
+    const port = process.env.PORT || 3000
+
+    const app = express();
+
+        app.listen(port, () => {
+            console.log(`Service ativo em http://localhost:${port}`);
+})
+
+app.use(express.json())
+
+Dicas.rotas(app);
